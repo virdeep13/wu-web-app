3a0e7cf44363ab4712a86d792fc33782e745cb4e
Virdeep <singhvirdeep@gmail.com>
2024-10-29 15:34:45 -0400

    Orgs

--- React_app/src/tabContent/Tab3Content.js
+++ React_app/src/tabContent/Tab3Content.js
@@ -1,7 +1,46 @@
 import React from 'react';
-
+import './App.css';
 const Tab3Content = () => {
-  return <div>Detailed content for Tab 3</div>;
-};
+  return( 
+  
+    <div className="tab3-container">
+    <h1>Organizations</h1>
+    <div className="search-bar">
+      <input type="text" placeholder="Search Organizations" />
+      <button>Categories</button>
+      <button>Options</button>
+    </div>
+    <div className="organization-grid">
+      <div className="organization-card">Active Minds</div>
+      <div className="organization-card">African Culture And Excellence (ACE)</div>
+      <div className="organization-card">Alpha Kappa Alpha Sorority, Inc. (AKA)</div>
+      <div className="organization-card">Alpha Omega Epsilon (AOE)</div>
+      <div className="organization-card">Alpha Phi Omega (APO)</div>
+      <div className="organization-card">Alpha Tau Omega (ATO)</div>
+      <div className="organization-card">American Institute of Chemical Engineers</div>
+      <div className="organization-card">American Society of Civil Engineers</div>
+      <div className="organization-card">Amnesty International</div>
+      
+  </div>
+;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+  );
+};
 export default Tab3Content;
