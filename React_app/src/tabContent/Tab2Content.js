68805ea0c0d03173d5f0e356de3ca45e949dcc27
Emaddeen Amm <emaddena@gmail.com>
2024-10-30 15:57:33 -0400

    roar?

--- React_app/src/tabContent/Tab2Content.js
+++ React_app/src/tabContent/Tab2Content.js
@@ -4,12 +4,12 @@ import Tabs from '../Tabs';
 const Tab2Content = () => {
   const nestedTabsData = [
   { title: 'Nested Tab 1', content: <div>Content for Nested Tab 1</div> },
-  { title: 'Nested Tab 1', content: <div>Content for Nested Tab 2</div> },
+  { title: 'Nested Tab 2', content: <div>Content for Nested Tab 2</div> },
   ];
 
   return( 
   <div>
-  <h2>detailed content for Tab1 </h2>
+  <h2>Events and announcements </h2>
   <Tabs tabs={nestedTabsData} />
 </div>
   );
